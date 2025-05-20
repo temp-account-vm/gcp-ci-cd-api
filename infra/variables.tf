a6e39d04ccbffbfa3df7f732ae23e4edb856570b
Quentin-Su <Quentin.Sussetto@outlook.fr>
2025-05-20 11:32:27 +0200

    fix: ("credentials.json")

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,9 +1,3 @@
-variable "project" {
-  default = "lateral-pillar-460313-d7"
-}
-variable "region" {
-  default = "europe-west1"
-}
-variable "zone" {
-  default = "europe-west1-b"
-}
+variable "project" {}
+variable "region" {}
+variable "zone" {}
